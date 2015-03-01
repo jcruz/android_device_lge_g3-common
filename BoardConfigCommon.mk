6e06f374d77dd7d8725cae808473927628ce0de9
777jon <jcruzdeveloper@gmail.com>
2015-03-01 13:04:39 -0500

    g3-common: TWRP bringup
    Signed-off-by: 777jon <jcruzdeveloper@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -13,17 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# inherit from the proprietary version
--include vendor/lge/g3-common/BoardConfigVendor.mk
-
-LOCAL_PATH := device/lge/g3-common
-
-TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
-
-# Platform
-TARGET_BOARD_PLATFORM := msm8974
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno330
-
 # CPU
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
@@ -38,60 +27,10 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8974
 TARGET_NO_BOOTLOADER := true
 
 # Kernel
-BOARD_CUSTOM_BOOTIMG := true
-BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=g3 user_debug=31 msm_rtb.filter=0x0 androidboot.bootdevice=msm_sdcc.1
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x0008000 --ramdisk_offset 0x2000000
-TARGET_KERNEL_SOURCE := kernel/lge/g3
-
-# Audio
-BOARD_USES_ALSA_AUDIO := true
-AUDIO_FEATURE_ENABLED_HWDEP_CAL := true
-AUDIO_FEATURE_LOW_LATENCY_PRIMARY := true
-
-# Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-
-# Camera
-USE_DEVICE_SPECIFIC_CAMERA := true
-COMMON_GLOBAL_CFLAGS += -DLG_CAMERA_HARDWARE
-COMMON_GLOBAL_CFLAGS += -DPROPERTY_PERMS_APPEND=' \
-    { "persist.data.sensor_name", AID_CAMERA, 0 }, \
-    { "camera.4k2k.enable", AID_MEDIA, 0 }, \
-    { "persist.data.rear.minfps", AID_MEDIA, 0 }, \
-    { "persist.data.front.minfps", AID_MEDIA, 0 }, \
-    '
-
-# CMHW
-BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/cmhw/
-
-# Display
-BOARD_EGL_CFG := $(LOCAL_PATH)/configs/egl.cfg
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
-TARGET_USES_C2D_COMPOSITION := true
-TARGET_USES_ION := true
-USE_OPENGL_RENDERER := true
-
-MAX_EGL_CACHE_KEY_SIZE := 12*1024
-MAX_EGL_CACHE_SIZE := 2048*1024
-
-HAVE_ADRENO_SOURCE:= false
-OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
-
-# Fonts
-EXTENDED_FONT_FOOTPRINT := true
-
-# Lights
-TARGET_PROVIDES_LIBLIGHT := true
-
-# Logging
-TARGET_USES_LOGD=false
-
-# NFC
-BOARD_NFC_CHIPSET := pn547
 
 # Offmode Charging
 COMMON_GLOBAL_CFLAGS += \
@@ -99,60 +38,29 @@ COMMON_GLOBAL_CFLAGS += \
     -DBOARD_CHARGING_CMDLINE_VALUE='"chargerlogo"'
 
 # Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2692743168
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 27325360128
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-# Power
-TARGET_POWERHAL_VARIANT := qcom
-
-# Qualcomm support
-BOARD_USES_QCOM_HARDWARE := true
-
-# Recovery
-BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_RECOVERY_SWIPE := true
-BOARD_SUPPRESS_EMMC_WIPE := true
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+# TWRP
 TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 
-# Radio
-BOARD_RIL_CLASS := ../../../device/lge/g3-common/ril/
+DEVICE_RESOLUTION := 1440x2560
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_NO_USB_STORAGE := true
+TW_INCLUDE_L_CRYPTO := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TARGET_RECOVERY_INITRC := device/lge/g3-common/twrp/init.rc
 
-# SELinux
-include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += device/lge/g3-common/sepolicy
+# Add tap to wake init
+PRODUCT_COPY_FILES += device/lge/g3-common/twrp/init.recovery.taptowake.rc:recovery/root/etc/init.recovery.taptowake.rc
 
-BOARD_SEPOLICY_UNION += \
-    device.te \
-    file.te \
-    file_contexts \
-    genfs_contexts \
-    hostapd.te \
-    init_shell.te \
-    kcal_dev.te \
-    kernel.te \
-    keystore.te \
-    lge_touch_sysfs.te \
-    mm-pp-daemon.te \
-    mm-qcamerad.te \
-    mpdecision.te \
-    nfc.te \
-    platform_app.te \
-    property.te \
-    property_contexts \
-    radio.te \
-    rmt_storage.te \
-    sensors.te \
-    servicemanager.te \
-    sysinit.te \
-    system_app.te \
-    tee.te \
-    thermal-engine.te \
-    time_data_file.te \
-    ueventd.te \
-    vold.te \
-    wcnss_service.te \
-    wpa.te
-
-# Time services
-BOARD_USES_QC_TIME_SERVICES := true
+# Recovery fstab
+PRODUCT_COPY_FILES += device/lge/g3-common/twrp/twrp.fstab:recovery/root/etc/twrp.fstab
